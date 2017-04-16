acdcd10ed5eee10e5774837c4d0068b559734857
unknown <jhonny_lns@hotmail.com>
2017-04-16 02:40:05 -0500

    Claseteclado

---
+++ 2014150721/2014150721/Teclado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014150721
+{
+    class Teclado
+    {
+        private decimal _MontoTeclado;
+
+        public decimal MontoTeclado
+        {
+            get
+            {
+                return _MontoTeclado;
+            }
+            set
+            {
+                if (value > 0)
+                    _MontoTeclado = value;
+            }
+        }
+
+        public Teclado(decimal montoTeclado)
+        {
+            MontoTeclado = montoTeclado;
+        }
+
+
+
+    }
+}
